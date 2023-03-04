c4f1f2146b60e3c1fcfb368bd20befe45274b3c3
Alex Kong <alex.lt.kong@connect.ust.hk>
2023-03-04 16:18:32 +0800

    migrate to use spdlog

--- src/camera-server/main.cpp
+++ src/camera-server/main.cpp
@@ -1,33 +1,36 @@
 #include <iostream>
 #include <signal.h>
-#include "motionDetector.h"
-#include "logger.h"
 
-logger myLogger = logger("/var/log/ak-studio/motion-detector/motionDetector.log", false);
+#include <spdlog/spdlog.h>
+
+#include "motionDetector.h"
+
+
 volatile sig_atomic_t done = 0;
 
 void signalCallbackHandler(int signum) {
-  if (signum == SIGPIPE) {    
-    return;
-  }
-  myLogger.info("main", "Signal " + std::to_string(signum) + " caught\n");
-  done = 1;
+    if (signum == SIGPIPE) {    
+        return;
+    }
+
+    spdlog::warn("Signal: {} caught, all threads will quit gracefully", signum);
+    done = 1;
 };
 
-int main() {  
-  myLogger.info("main", "motionDetector started");
+int main() {
+    spdlog::set_pattern("%Y-%m-%dT%T%z | %t | %8l | %v");
+    spdlog::info("Camera Server started");
 
-  struct sigaction act;
-  act.sa_handler = signalCallbackHandler;
-  sigemptyset(&act.sa_mask);
-  act.sa_flags = SA_RESETHAND;
-  sigaction(SIGINT, &act, 0);
-  sigaction(SIGABRT, &act, 0);
-  sigaction(SIGTERM, &act, 0);
-  sigaction(SIGPIPE, &act, 0);
+    struct sigaction act;
+    act.sa_handler = signalCallbackHandler;
+    sigemptyset(&act.sa_mask);
+    act.sa_flags = SA_RESETHAND;
+    sigaction(SIGINT, &act, 0);
+    sigaction(SIGABRT, &act, 0);
+    sigaction(SIGTERM, &act, 0);
+    sigaction(SIGPIPE, &act, 0);
 
-  motionDetector* myDetector = new motionDetector(&done);
-  myDetector->main();
-  delete myDetector;
-  return 0;  
+    motionDetector myDetector = motionDetector(&done);
+    myDetector.main();
+    return 0;  
 }
