bfaf3bcfbbc8e57b5399484732e878a251a2b3ca
Alex Kong <alex.lt.kong@connect.ust.hk>
2023-10-27 00:52:44 +0800

    http auth + image serving mostly okay

--- src/camera-server/http_service/auth_handler.h
+++ src/camera-server/http_service/auth_handler.h
@@ -1,40 +1,52 @@
-#ifndef CS_AUTH_HANDLER_HPP
-#define CS_AUTH_HANDLER_HPP
+#ifndef CS_AUTH_HANDLER_H
+#define CS_AUTH_HANDLER_H
+
+#include "../global_vars.h"
 
 #include <oatpp/core/Types.hpp>
 #include <oatpp/core/base/Environment.hpp>
 #include <oatpp/web/server/handler/AuthorizationHandler.hpp>
+#include <spdlog/spdlog.h>
 
 #include <iostream>
 
-namespace owsh = oatpp::web::server::handler;
+typedef oatpp::String oString;
+typedef oatpp::web::server::handler::BasicAuthorizationHandler
+    BasicAuthorizationHandler;
+typedef oatpp::web::server::handler::AuthorizationObject AuthorizationObject;
 
-class MyAuthorizationObject : public owsh::AuthorizationObject {
+class MyAuthorizationObject : public AuthorizationObject {
 public:
-  oatpp::String User;
+  oString User;
 
   static std::shared_ptr<MyAuthorizationObject> createShared() {
     return std::make_shared<MyAuthorizationObject>();
   }
 };
 
-class MyAuthorizationHandler : public owsh::BasicAuthorizationHandler {
+class MyAuthorizationHandler : public BasicAuthorizationHandler {
 public:
-  MyAuthorizationHandler()
-      : owsh::BasicAuthorizationHandler(PROJECT_NAME "-realm") {}
+  MyAuthorizationHandler() : BasicAuthorizationHandler(PROJECT_NAME "-realm") {}
 
   // override `authorize` function with your own logic
   // `authorize` gets called with the token received in the bearer-authorization
   // header.
-  std::shared_ptr<AuthorizationObject>
-  authorize(const oatpp::String &userId, const oatpp::String &password) {
-    if (userId == "foo" && password == "bar") {
-      auto authObject = MyAuthorizationObject::createShared();
-      authObject->User = "foo";
-      return authObject;
+  std::shared_ptr<AuthorizationObject> authorize(const oString &userId,
+                                                 const oString &password) {
+    if (!settings["httpService"]["httpAuthentication"]["accounts"].contains(
+            userId->c_str())) {
+      spdlog::warn("User [{}] does not exist", userId->c_str());
+      return nullptr;
     }
-    return nullptr;
+    if (settings["httpService"]["httpAuthentication"]["accounts"]
+                [userId->c_str()] != password->c_str()) {
+      spdlog::warn("password is incorrect for user [{}]", password->c_str());
+      return nullptr;
+    }
+    auto authObject = MyAuthorizationObject::createShared();
+    authObject->User = userId;
+    return authObject;
   }
 };
 
-#endif /* CS_AUTH_HANDLER_HPP */
+#endif /* CS_AUTH_HANDLER_H */
--- src/camera-server/http_service/controller/StaticController.hpp
+++ src/camera-server/http_service/controller/StaticController.hpp
@@ -19,6 +19,7 @@
 typedef oatpp::web::protocol::http::outgoing::BufferBody BufferBody;
 typedef oatpp::web::protocol::http::outgoing::Response Response;
 typedef oatpp::web::server::api::ApiController ApiController;
+typedef oatpp::String String;
 
 class StaticController : public ApiController {
 public:
@@ -40,15 +41,15 @@ public:
 
     std::cout << authObject->User->c_str() << std::endl;
 
-    oatpp::String html = "<html lang='en'>"
-                         "  <head>"
-                         "    <meta charset=utf-8/>"
-                         "  </head>"
-                         "  <body>"
-                         "    <p>" PROJECT_NAME "</p>"
-                         "    <a href='swagger/ui'>Checkout Swagger-UI page</a>"
-                         "  </body>"
-                         "</html>";
+    String html = "<html lang='en'>"
+                  "  <head>"
+                  "    <meta charset=utf-8/>"
+                  "  </head>"
+                  "  <body>"
+                  "    <p>" PROJECT_NAME "</p>"
+                  "    <a href='swagger/ui'>Checkout Swagger-UI page</a>"
+                  "  </body>"
+                  "</html>";
     html->append(authObject->User);
     auto response = createResponse(Status::CODE_200, html);
     response->putHeader(Header::CONTENT_TYPE, "text/html");
@@ -61,31 +62,25 @@ public:
 
     /* get url 'tail' - everything that comes after '*' */
     String tail = request->getPathTail(); // everything that goes after '*'
-
     /* check tail for null */
     OATPP_ASSERT_HTTP(tail, Status::CODE_400, "null query-params");
-
-    /* parse query params from tail */
     auto queryParams = oatpp::network::Url::Parser::parseQueryParams(tail);
 
     /* get your param by name */
     auto queryParameter = queryParams.get("deviceId");
-    uint32_t deviceId = 0;
+    size_t deviceId = 0;
     if (queryParameter != nullptr) {
-      spdlog::info("{}", queryParameter->c_str());
       deviceId = atoi(queryParameter->c_str());
+      if (deviceId < myDevices.size()) {
+        std::vector<uint8_t> encodedImg;
+        myDevices[deviceId]->getLiveImage(encodedImg);
+        String buf = String((const char *)encodedImg.data(), encodedImg.size());
+        return Response::createShared(Status::CODE_200,
+                                      BufferBody::createShared(buf));
+      }
     }
-    if (deviceId < myDevices.size()) {
-      std::vector<uint8_t> encodedImg;
-      myDevices[deviceId]->getLiveImage(encodedImg);
-      oatpp::String buf =
-          oatpp::String((const char *)encodedImg.data(), encodedImg.size());
-      auto body = BufferBody::createShared(buf);
-      return Response::createShared(Status::CODE_200, body);
-    } else {
-      return Response::createShared(Status::CODE_200,
-                                    BufferBody::createShared("ERROR"));
-    }
+    return Response::createShared(Status::CODE_200,
+                                  BufferBody::createShared("ERROR"));
   }
 };
 
